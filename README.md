03d1776827173062bf555a9034f8343abcdac7d8
juan.rodriguez <juan.rodriguez@patagoniansys.com>
2022-07-15 17:30:00 -0300

    RecyclerView implementation

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-<b>NewApp v1.0</b><br>
+<b>NewApp v1.1</b><br>
 <br>
 It doesn't have a name because it was created for you to give it one!<br>
 This app makes a simple API call and shows the result. Probably something that someone asked you to do for an interview...
@@ -10,6 +10,5 @@ Base kotlin Android app usign:
 - Architecture components (Viewmodel and LiveData)
 
 Soon (!):
-- Better UI (List of elements and details)
-- Nav component
+- Navigation to details with Nav component
 - Unit tests
--- app/src/main/java/com/example/newapp/presentation/view/MainActivity.kt
+++ app/src/main/java/com/example/newapp/presentation/view/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.newapp.presentation.view
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.newapp.R
 import com.example.newapp.presentation.model.MovieListState
 import com.example.newapp.presentation.viewmodel.MovieListViewModel
@@ -15,17 +18,22 @@ import javax.inject.Inject
 class MainActivity : AppCompatActivity() {
 
     private val viewModel by viewModels<MovieListViewModel>()
+    private lateinit var recyclerAdapter: CustomAdapter
 
     private val stateObserver = Observer<MovieListState> { state ->
         when (state) {
             is MovieListState.Loading -> {
-                mainText.text = "Loading..."
+                progressIndicator.visibility = View.VISIBLE
             }
             is MovieListState.Success -> {
-                mainText.text = state.data.first().name
+                progressIndicator.visibility = View.GONE
+                recyclerView.layoutManager = GridLayoutManager(applicationContext, 2)
+                recyclerAdapter = CustomAdapter(applicationContext, state.data)
+                recyclerView.adapter = recyclerAdapter
             }
             is MovieListState.Error -> {
-                mainText.text = state.errorMessage
+                progressIndicator.visibility = View.GONE
+                Log.e("BASE APP", state.errorMessage)
             }
         }
     }
@@ -33,9 +41,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         observeViewModel()
-        savedInstanceState?.let {
-            viewModel.loadMovies()
-        } ?: viewModel.loadMovies()
+        viewModel.loadMovies()
         setContentView(R.layout.activity_main)
     }
 
